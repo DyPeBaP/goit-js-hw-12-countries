46efbf6a75e234aeb6f6c042c499488242360ea7
DyPeBaP <66387850+DyPeBaP@users.noreply.github.com>
2021-08-28 16:58:51 +0300

    alert

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-import './sass/main.scss';
-import '@pnotify/core/dist/BrightTheme.css';
-import { notice, error, Stack } from '@pnotify/core';
+import "./sass/main.scss";
+import "@pnotify/core/dist/BrightTheme.css";
+import { notice, error, Stack } from "@pnotify/core";
 
-import fetchCountries from './fetchCountries';
-import countryListMarkup from './templates/countriesList.hbs';
-import countryCardMarkup from './templates/countryCard.hbs';
+import fetchCountries from "./fetchCountries";
+import countryListMarkup from "./templates/countriesList.hbs";
+import countryCardMarkup from "./templates/countryCard.hbs";
 
-const debounce = require('lodash.debounce');
+const debounce = require("lodash.debounce");
 const refs = {
-  input: document.querySelector('.input__text'),
-  renderBox: document.querySelector('.render_box'),
+  input: document.querySelector(".input__text"),
+  renderBox: document.querySelector(".render_box"),
   debouncedOnInput: debounce(onInput, 1000),
 };
 
-refs.input.addEventListener('input', refs.debouncedOnInput);
+refs.input.addEventListener("input", refs.debouncedOnInput);
 
 function onInput(e) {
   const country = e.target.value;
-  fetchCountries(country).then(r => console.log(r));
+  fetchCountries(country).then((r) => console.log(r));
   fetchCountries(country)
-    .then(r => renderResult(r))
+    .then((r) => renderResult(r))
+    .catch(() => noticeMessage(alert))
     .catch(() => errorServerMessage());
 }
 
@@ -47,32 +48,32 @@ function createCountryCardMarkup(arr) {
 }
 
 function noticeMessage() {
-  refs.renderBox.innerHTML = '';
+  refs.renderBox.innerHTML = "";
   notice({
-    title: 'Attention',
-    text: 'To many results, try to specify your search.',
-    width: '300px',
-    minHeight: '15px',
+    title: "Attention",
+    text: "To many results, try to specify your search.",
+    width: "300px",
+    minHeight: "15px",
     delay: 2000,
   });
 }
 function errorMessage() {
-  refs.renderBox.innerHTML = '';
+  refs.renderBox.innerHTML = "";
   error({
-    title: 'Error',
-    text: 'No matchs found!',
-    width: '300px',
-    minHeight: '15px',
+    title: "Error",
+    text: "No matchs found!",
+    width: "300px",
+    minHeight: "15px",
     delay: 2000,
   });
 }
 function errorServerMessage() {
-  refs.renderBox.innerHTML = '';
+  refs.renderBox.innerHTML = "";
   error({
-    title: 'Error',
-    text: 'Some things goes wrong, try again later',
-    width: '300px',
-    minHeight: '15px',
+    title: "Error",
+    text: "Some things goes wrong, try again later",
+    width: "300px",
+    minHeight: "15px",
     delay: 2000,
   });
 }
